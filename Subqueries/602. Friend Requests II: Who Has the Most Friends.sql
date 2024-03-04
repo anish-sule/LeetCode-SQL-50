b910705d71b2f61bc331a8f80a8ca9bc23610cf7
anish sule <159512498+anish-sule@users.noreply.github.com>
2024-03-04 15:01:08 +0530

    Create 602. Friend Requests II: Who Has the Most Friends.sql

---
+++ Subqueries/602. Friend Requests II: Who Has the Most Friends.sql
@@ -0,0 +1,67 @@
+-- QUESTION :
+
+/*
+Table: RequestAccepted
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| requester_id   | int     |
+| accepter_id    | int     |
+| accept_date    | date    |
++----------------+---------+
+(requester_id, accepter_id) is the primary key (combination of columns with unique values) for this table.
+This table contains the ID of the user who sent the request, the ID of the user who received the request, and the date when the request was accepted.
+
+Write a solution to find the people who have the most friends and the most friends number.
+The test cases are generated so that only one person has the most friends.
+
+Example 1:
+
+Input: 
+RequestAccepted table:
++--------------+-------------+-------------+
+| requester_id | accepter_id | accept_date |
++--------------+-------------+-------------+
+| 1            | 2           | 2016/06/03  |
+| 1            | 3           | 2016/06/08  |
+| 2            | 3           | 2016/06/08  |
+| 3            | 4           | 2016/06/09  |
++--------------+-------------+-------------+
+Output: 
++----+-----+
+| id | num |
++----+-----+
+| 3  | 3   |
++----+-----+
+Explanation: 
+The person with id 3 is a friend of people 1, 2, and 4, so he has three friends in total, which is the most number than any others.
+*/
+
+
+
+-- SOLUTION (MySQL)
+
+SELECT r.id, SUM(r.num) num
+FROM
+    (
+    SELECT requester_id id, COUNT(requester_id) num FROM RequestAccepted GROUP BY requester_id
+    UNION ALL
+    SELECT accepter_id id, COUNT(accepter_id) num  FROM RequestAccepted GROUP BY accepter_id
+    ) r
+GROUP BY r.id 
+ORDER BY num  DESC
+LIMIT 1
+
+
+
+-- SOLUTION (MS SQL Server)
+
+SELECT TOP 1 r.id, SUM(r.num) num
+FROM
+    (
+    SELECT requester_id id, COUNT(requester_id) num FROM RequestAccepted GROUP BY requester_id
+    UNION ALL
+    SELECT accepter_id id, COUNT(accepter_id) num  FROM RequestAccepted GROUP BY accepter_id
+    ) r
+GROUP BY r.id 
+ORDER BY num  DESC
